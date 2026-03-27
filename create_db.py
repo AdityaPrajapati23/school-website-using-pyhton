16b7fb05f3b541f4b9e4ab8956ed0deb59ae57ca
Aditya Prajapati23 <adityaprajapati61000@gmail.com>
2026-03-27 19:16:37 +0530

    school website

---
+++ create_db.py
@@ -0,0 +1,64 @@
+import sqlite3
+
+conn = sqlite3.connect('database.db')
+cursor = conn.cursor()
+
+# Students table
+cursor.execute('''
+CREATE TABLE IF NOT EXISTS students (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT,
+    dob TEXT,
+    class TEXT,
+    father TEXT,
+    phone TEXT
+)
+''')
+
+# Users table for login
+cursor.execute('''
+CREATE TABLE IF NOT EXISTS users (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    username TEXT,
+    password TEXT,
+    role TEXT
+)
+''')
+cursor.execute('''
+CREATE TABLE IF NOT EXISTS notices (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    notice TEXT
+)
+''')
+cursor.execute('''
+CREATE TABLE IF NOT EXISTS attendance (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    student_id INTEGER,
+    date TEXT,
+    status TEXT
+)
+''')
+
+cursor.execute('''
+CREATE TABLE IF NOT EXISTS results (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    student_id INTEGER,
+    subject TEXT,
+    marks INTEGER
+)
+''')
+
+
+cursor.execute('''
+CREATE TABLE IF NOT EXISTS fees (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    student_id INTEGER,
+    amount INTEGER,
+    status TEXT
+)
+''')
+
+conn.commit()
+conn.close()
+
+print("Database Ready Successfully")
